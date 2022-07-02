c7b9ca0223f9b0ed89d98603b94eb1da1eeb3abe
Martin Ruiz Diaz <martinrd.1606@gmail.com>
2022-07-02 19:07:25 -0300

    POO

--- script.js
+++ script.js
@@ -1,36 +1,61 @@
-function plus(){
-    var n1 = parseInt(document.getElementById('n1').value);
-    var n2 = parseInt(document.getElementById('n2').value);
-    let c;
-    c = (n1 + n2);
-    document.getElementById("resul").innerHTML = c;
+const plus =  document.querySelector('#plus');
+const minus =  document.querySelector('#minus');
+const multi =  document.querySelector('#multi');
+const division =  document.querySelector('#division');
+
+const n1 = document.querySelector('#n1');
+const n2 = document.querySelector('#n2');
+
+class calculadora {
+    #number1;
+    #number2;
+    constructor (number1,number2){
+        this.#number1 = number1;
+        this.#number2 = number2;
+    }
+    getPlus(){
+        return this.#number1 + this.#number2;  
+    }
+    getMinus(){
+        return this.#number1 - this.#number2;  
+    }
+    getMulti(){
+        return this.#number1 * this.#number2;  
+    }
+    getDivision(){
+        return this.#number1 / this.#number2;  
+    }
+
 }
 
-function minus(){
-    var n1 = parseInt(document.getElementById('n1').value);
-    var n2 = parseInt(document.getElementById('n2').value);
-    let c;
-    c = (n1 - n2);
-    document.getElementById("resul").innerHTML = c;
-}
 
-function multi(){
-    var n1 = parseInt(document.getElementById('n1').value);
-    var n2 = parseInt(document.getElementById('n2').value);
-    let c;
-    c = (n1 * n2);
-    document.getElementById("resul").innerHTML = c;
-}
+plus.addEventListener('click', function(){
+    const num1 = parseFloat(n1.value);
+    const num2 = parseFloat(n2.value);
+    const minus = new calculadora(num1, num2);
+    document.querySelector('#resul').innerHTML = minus.getPlus();
+})
+minus.addEventListener('click', function(){
+    const num1 = parseFloat(n1.value);
+    const num2 = parseFloat(n2.value);
+    const minus = new calculadora(num1, num2);
+    document.querySelector('#resul').innerHTML = minus.getMinus();
+})
+multi.addEventListener('click', function(){
+    const num1 = parseFloat(n1.value);
+    const num2 = parseFloat(n2.value);
+    const minus = new calculadora(num1, num2);
+    document.querySelector('#resul').innerHTML = minus.getMulti();
+})
+division.addEventListener('click', function(){
+    const num1 = parseFloat(n1.value);
+    const num2 = parseFloat(n2.value);
+    const minus = new calculadora(num1, num2);
+    document.querySelector('#resul').innerHTML = minus.getDivision();
+})
 
-function division(){
-    var n1 = parseInt(document.getElementById('n1').value);
-    var n2 = parseInt(document.getElementById('n2').value);
-    let c;
-    c = (n1 / n2);
-    document.getElementById("resul").innerHTML = c;
-}
-
-function limpar(){
-    document.getElementById('n1').value = ''
-    document.getElementById('n2').value = ''
+function limpar(){    
+    n1.value = '';
+    n2.value = '';
+    document.querySelector('#resul').textContent = '';
 }
